994360a0ed63a92f1a1ce3d193ed45b72da4d8a3
Zach Tucker <zbtucker@gmail.com>
2020-12-04 08:25:57 -0700

    basic express route

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const products = require('./data/products');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('API is running');
+})
+
+app.get('/api/products', (req, res) => {
+    res.json(products);
+})
+
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find(id => id._id == req.params.id);
+    res.json(product);
+})
+
+app.listen(5000, () => (
+    console.log(`Server listening on port 5000`)
+));
