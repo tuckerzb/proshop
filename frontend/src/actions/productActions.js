d44d6eb21dbf2b8deece44da12ba7bdadf58c8a4
Zach Tucker <zbtucker@gmail.com>
2020-12-05 20:31:47 -0700

    implemented product details reducer

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,4 +1,4 @@
-import {PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAILURE} from '../constants/productConstants';
+import {PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAILURE, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAILURE} from '../constants/productConstants';
 import axios from 'axios';
 
 export const listProducts = () => async (dispatch) => {
@@ -8,5 +8,15 @@ export const listProducts = () => async (dispatch) => {
         dispatch({type: PRODUCT_LIST_SUCCESS, payload: data});
     } catch (error) {
         dispatch({type: PRODUCT_LIST_FAILURE, payload: error.response && error.response.data.message ? error.response.data.message : error.message});
+    }
+}
+
+export const listProductDetails = (id) => async (dispatch) => {
+    try {
+        dispatch({type: PRODUCT_DETAILS_REQUEST});
+        const {data} = await axios.get(`/api/products/${id}`);
+        dispatch({type: PRODUCT_DETAILS_SUCCESS, payload: data});
+    } catch (error) {
+        dispatch({type: PRODUCT_DETAILS_FAILURE, payload: error.response && error.response.data.message ? error.response.data.message : error.message});
     }
 }
--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,4 +1,4 @@
-import {PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAILURE} from '../constants/productConstants';
+import {PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAILURE, PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAILURE} from '../constants/productConstants';
 
 export const productListReducer = (state = {products: []}, action) => {
     switch (action.type) {
@@ -9,6 +9,23 @@ export const productListReducer = (state = {products: []}, action) => {
             return {loading: false, products: action.payload};
             break;
         case PRODUCT_LIST_FAILURE:
+            return {loading: false, error: action.payload};
+            break;
+        default:
+            return state;
+            break;
+    }
+}
+
+export const productDetailsReducer = (state = {products: {reviews: []}}, action) => {
+    switch (action.type) {
+        case PRODUCT_DETAILS_REQUEST:
+            return {loading: true, ...state};
+            break;
+        case PRODUCT_DETAILS_SUCCESS:
+            return {loading: false, product: action.payload};
+            break;
+        case PRODUCT_DETAILS_FAILURE:
             return {loading: false, error: action.payload};
             break;
         default:
