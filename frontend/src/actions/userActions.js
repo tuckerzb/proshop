783737af61148ff58ccd312e6f9f00ebdaf14e1a
Zach Tucker <zbtucker@gmail.com>
2020-12-12 21:00:52 -0700

    implemented user logout

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,4 +1,4 @@
-import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAILURE } from "../constants/userConstants"
+import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAILURE, USER_LOGOUT } from "../constants/userConstants"
 import axios from 'axios';
 
 export const login = (email, password) => async (dispatch) => {
@@ -22,4 +22,11 @@ export const login = (email, password) => async (dispatch) => {
         dispatch({type: USER_LOGIN_FAILURE, payload: error.response && error.response.data.message ? error.response.data.message : error.message});
 
     }
+}
+
+export const logout = () => (dispatch) => {
+    localStorage.removeItem('userInfo');
+    dispatch({
+        type: USER_LOGOUT
+    })
 }
