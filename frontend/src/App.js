5ce0af8c36619b32ceabc32e9a21a4cb3f61b6b3
Zach Tucker <zbtucker@gmail.com>
2020-12-17 20:37:08 -0700

    implemented order status screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import ProfileScreen from './screens/ProfileScreen';
 import ShippingScrreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
 
 const App = () => {
   return (
@@ -26,7 +27,8 @@ const App = () => {
           <Route path='/payment' component={PaymentScreen} />
           <Route path='/placeorder' component={PlaceOrderScreen} />
           <Route path='/product/:id' component={ProductScreen} />
-          <Route path='/cart/:id?' component={CartScreen} />
+          <Route path='/cart/:id' component={CartScreen} />
+          <Route path='/order/:id' component={OrderScreen} />
           <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -1,4 +1,4 @@
-import {ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_CREATE_FAILURE} from '../constants/orderConstants';
+import {ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_CREATE_FAILURE, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_DETAILS_FAILURE} from '../constants/orderConstants';
 
 export const orderCreateReducer = (state = {}, action) => {
     switch (action.type) {
@@ -9,6 +9,23 @@ export const orderCreateReducer = (state = {}, action) => {
             return {loading: false, success: true, order: action.payload};
             break;
         case ORDER_CREATE_FAILURE:
+            return {loading: false, error: action.payload};
+            break;
+        default:
+            return state;
+            break;
+    }
+}
+
+export const orderDetailsReducer = (state = {loading: true, orderItems: [], shippingAddress: {}}, action) => {
+    switch (action.type) {
+        case ORDER_DETAILS_REQUEST:
+            return {...state, loading: true};
+            break;
+        case ORDER_DETAILS_SUCCESS:
+            return {loading: false, order: action.payload};
+            break;
+        case ORDER_DETAILS_FAILURE:
             return {loading: false, error: action.payload};
             break;
         default:
