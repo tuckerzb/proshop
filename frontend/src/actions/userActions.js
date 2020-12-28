7904cfe18797b2d420198d7ab5956ffb6febb5e4
Zach Tucker <zbtucker@gmail.com>
2020-12-27 20:11:19 -0700

    fixed header profile name bug

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -109,6 +109,11 @@ export const updateUserProfile = (user) => async (dispatch, getState) => {
             type: USER_UPDATE_PROFILE_SUCCESS,
             payload: data
         });
+        dispatch({
+            type: USER_LOGIN_SUCCESS,
+            payload: data
+        });
+        localStorage.setItem('userInfo', JSON.stringify(data));
     } catch (error) {
         dispatch({type: USER_UPDATE_PROFILE_FAILURE, payload: error.response && error.response.data.message ? error.response.data.message : error.message});
 
